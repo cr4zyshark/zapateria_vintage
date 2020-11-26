ed5e5ddf4bed0ee1f0b09617be91a17bae7484ce
jose pino <josepfuentes22@gmail.com>
2020-11-26 16:42:36 -0300

    empezando proyecto en java

---
+++ proyecto_zapateria/src/Clases/Conexion.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author Jose
+ */
+//public class Conexion {
+//    
+//    Connection Conectar;
+//    private String host = "Localhost";
+//    private String port = "3306";
+//    private String base_datos = "zapateria";
+//    private String username = "Root";
+//    private String pass = "123";
+//    
+//    public Conexion(){
+//        try{
+//            Class.forName("com.mysql.jdbc.Driver");
+//            String url = "jdbc:mysql://" + this.host + ":" + this.port + "/" + this.base_datos;
+//            Conectar = DriverManager.getConnection(url, this.username, this.pass);
+//            System.out.println("conexion exitosa");
+//            
+//        
+//        }catch (ClassNotFoundExcepcion SQLException e){
+//            System.out.println("error");
+//        
+//        }
+//        
+//    
+//    
+//    
+//    }
+//    }
+//}
+//    
+    
+            
+    
+
